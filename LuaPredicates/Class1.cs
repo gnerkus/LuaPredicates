9485c86534172512daf2ab1985d0bc1e4274a125
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-11-01 21:20:26 +0100

    add NLua dependency

--- LuaPredicates/Class1.cs
+++ LuaPredicates/Class1.cs
@@ -1,5 +1,28 @@
-﻿namespace LuaPredicates;
+﻿using NLua;
 
-public class Class1
+namespace LuaPredicates
 {
+    public class Class1
+    {
+        public void TestParse()
+        {
+            var state = new Lua();
+            state.DoString("""
+                           
+                           		function ScriptFunc (val1, val2)
+                           			if val1 > val2 then
+                           				return val1 + 1
+                           			else
+                           				return val2 - 1
+                           			end
+                           		end
+                           		
+                           """);
+            var scriptFunc = state["ScriptFunc"] as LuaFunction;
+            var res = (int)scriptFunc.Call(3, 5).First();
+            // LuaFunction.Call will also return a array of objects, since a Lua function
+            // can return multiple values
+            Console.WriteLine(res);
+        }
+    }
 }
