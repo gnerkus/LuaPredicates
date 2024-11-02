8e868b17aec39711f396ea8c373a59a9c1303326
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-11-02 16:16:43 +0100

    add extension method to read lua script from string

---
+++ LuaPredicates/LuaPredicateExtensions.cs
@@ -0,0 +1,16 @@
+﻿using NLua;
+
+namespace LuaPredicates
+{
+    public static class LuaPredicateExtensions
+    {
+        public static Func<TSource, TResult> ReadFromString<TSource, TResult>(this 
+                ILuaPredicate<TSource, TResult> 
+                luaPredicate, string luaSource)
+        {
+            var state = new Lua();
+            state.DoString(luaSource);
+            return luaPredicate.GetFunction(state);
+        }
+    }
+}
