600e26013506872a10600d8b89ed7a213eb5db96
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-11-02 16:00:25 +0100

    get a function from a Lua object

---
+++ LuaPredicates/ILuaPredicate.cs
@@ -0,0 +1,9 @@
+﻿using NLua;
+
+namespace LuaPredicates
+{
+    public interface ILuaPredicate<TSource, TResult>
+    {
+        Func<TSource, TResult> GetFunction(Lua scriptSource);
+    }
+}
--- LuaPredicates/LuaPredicate.cs
+++ LuaPredicates/LuaPredicate.cs
@@ -2,11 +2,22 @@
 
 namespace LuaPredicates
 {
-    public class LuaPredicate<TEntity>
+    public class LuaPredicate<TSource, TResult>: ILuaPredicate<TSource, TResult>
     {
         public LuaPredicate()
         {
             
+        }
+
+
+        public Func<TSource, TResult> GetFunction(Lua scriptSource)
+        {
+            ArgumentNullException.ThrowIfNull(scriptSource);
+
+            // TODO: decide on the name of the function
+            var scriptFunc = scriptSource["Calculate"] as LuaFunction;
+            return sourceEntity => (TResult)scriptFunc.Call
+                (sourceEntity).First();
         }
 
         public override string ToString()
