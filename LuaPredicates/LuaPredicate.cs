48d8dadf19aaab1bc7663d55f7d7a023fe6bc926
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-11-02 12:09:04 +0100

    rename main class

---
+++ LuaPredicates/LuaPredicate.cs
@@ -0,0 +1,17 @@
+﻿using NLua;
+
+namespace LuaPredicates
+{
+    public class LuaPredicate<TEntity>
+    {
+        public LuaPredicate()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return $"LuaPredicate";
+        }
+    }
+}
